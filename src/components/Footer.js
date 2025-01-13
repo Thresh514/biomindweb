deef2fb0935793cbaf53b6dac92e4c2b3d5c8c28
Thresh514 <165234885+Thresh514@users.noreply.github.com>
2025-01-13 00:08:58 -0500

    Your message here

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer(){
     return (
         <footer className="flex bg-gray-100 text-sm p-4 mt-4 justify-between fixed bottom-0 left-0 right-0">
             <p>© 2025 Biomind Logic. All rights reserved.</p>
-            <p>Questions or feedback? Contact us at:tonytudaodao@gmail.com</p>
+            <p>Questions or feedback? Contact us at: tonytudaodao@gmail.com</p>
         </footer>
     );
 }
