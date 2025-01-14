cd96183dbdf9f93f756ac0eafac22c3e6a626962
Thresh514 <165234885+Thresh514@users.noreply.github.com>
2025-01-14 17:34:40 -0500

    more content on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import HeroSection from '../components/Herosection';
 import ResourceCard from '../components/ResourceCard';
+import MembershipBenefits from '../components/MembershipBenefits';
+import SuccessStories from '../components/SuccessStories';
 
 
 export default function Home() {
@@ -16,8 +18,14 @@ export default function Home() {
       </Head>
       <Header/>
       <Navbar/>
-      <main className="pt-32">
-        <ResourceCard/>
+      <main className="pt-16 flex flex-col items-center justify-center">
+        <HeroSection/>
+        <ResourceCard />
+        <div className="mt-32 mb-32">
+          <MembershipBenefits/>
+          <SuccessStories/>
+        </div>
+
       </main>
 
       <Footer/>
