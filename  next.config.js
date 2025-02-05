b61b01d7e918ea3d092977c06a42663a86058637
Kunyu Zheng <kunyuz@bu.edu>
2025-02-05 00:27:19 -0500

    Updated next.config.js and db.js

---  next.config.js
+++  next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // 启用静态导出模式
+    //output: 'export', // 启用静态导出模式
 };
 
 module.exports = nextConfig;
--- src/lib/db.js
+++ src/lib/db.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 
 // 创建连接池，配置数据库连接信息
 export const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '20252025',
-  database: 'file',
+  database: 'bio_database',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
